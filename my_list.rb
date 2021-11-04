41770b00c1712c9747afdf7ce208a130e1aff222
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-04 16:43:59 +0100

    Create my_enumerables.rb file and Add MyEnumerable Module

--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,4 @@
-require_relative 'my_enumerable'
-
+require_relative 'my_enumerables'
 class MyList
   include MyEnumerable
 
@@ -11,3 +10,21 @@ class MyList
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+# true
+puts(list.all? { |e| e > 5 })
+# false
+
+# Test #any?
+puts(list.any? { |e| e == 2 })
+# true
+puts(list.any? { |e| e == 5 })
+# false
+
+# Test #filter
+puts list.filter(&:even?)
+# [2, 4]
